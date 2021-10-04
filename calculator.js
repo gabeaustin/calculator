c388721017636660aa3ee21495fe8c63a9de9614
gabeaustin <gabriel.austin2019@gmail.com>
2021-10-04 00:31:38 -0500

    added buttons

---
+++ calculator.js
@@ -0,0 +1,20 @@
+let add = add += 1;
+let subtract = subtract -= 1;
+let multiply = multiply *= 1;
+let divide = divide /= 1;
+
+function add() {
+
+}
+
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+    
+}
