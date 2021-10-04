78011645c0453781f16ce65424f045d6e181b32c
gabeaustin <gabriel.austin2019@gmail.com>
2021-10-04 00:52:12 -0500

    added numbers one and seven with the functions that go with these numbers

--- calculator.js
+++ calculator.js
@@ -1,20 +1,33 @@
-let add = add += 1;
-let subtract = subtract -= 1;
-let multiply = multiply *= 1;
-let divide = divide /= 1;
+let number = 0;
+// let add = add += 1;
+// let subtract = subtract -= 1;
+// let multiply = multiply *= 1;
+// let divide = divide /= 1;
+
+function oneButton() {
+    let oneDigit = 1;
+    document.getElementById("number-display").textContent = oneDigit;
+}
+
+function sevenButton() {
+    let sevenDigit = 7;
+    document.getElementById("number-display").textContent = sevenDigit;
+}
+
+
 
 function add() {
-
+    let addNumber = add += number;
 }
 
-function subtract() {
+// function subtract() {
 
-}
+// }
 
-function multiply() {
+// function multiply() {
 
-}
+// }
 
-function divide() {
-    
-}
+// function divide() {
+
+// }
