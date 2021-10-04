c81e48b50829686ba3af118aedb092ade9de0376
gabeaustin <gabriel.austin2019@gmail.com>
2021-10-04 01:00:03 -0500

    added onClick event for numbers 0-9

--- calculator.js
+++ calculator.js
@@ -5,19 +5,56 @@ let number = 0;
 // let divide = divide /= 1;
 
 function oneButton() {
-    let oneDigit = 1;
-    document.getElementById("number-display").textContent = oneDigit;
+  let oneDigit = 1;
+  document.getElementById("number-display").textContent = oneDigit;
+}
+
+function twoButton() {
+  let twoDigit = 2;
+  document.getElementById("number-display").textContent = twoDigit;
+}
+
+function threeButton() {
+  let threeDigit = 3;
+  document.getElementById("number-display").textContent = threeDigit;
+}
+
+function fourButton() {
+  let fourDigit = 4;
+  document.getElementById("number-display").textContent = fourDigit;
+}
+
+function fiveButton() {
+  let fiveDigit = 5;
+  document.getElementById("number-display").textContent = fiveDigit;
+}
+
+function sixButton() {
+  let sixDigit = 6;
+  document.getElementById("number-display").textContent = sixDigit;
 }
 
 function sevenButton() {
-    let sevenDigit = 7;
-    document.getElementById("number-display").textContent = sevenDigit;
+  let sevenDigit = 7;
+  document.getElementById("number-display").textContent = sevenDigit;
+}
+function eightButton() {
+  let eightDigit = 8;
+  document.getElementById("number-display").textContent = eightDigit;
 }
 
+function nineButton() {
+  let nineDigit = 9;
+  document.getElementById("number-display").textContent = nineDigit;
+}
 
+function zeroButton() {
+  let zeroDigit = 0;
+  document.getElementById("number-display").textContent = zeroDigit;
+}
 
 function add() {
-    let addNumber = add += number;
+  let addNumber = (add += number);
 }
 
 // function subtract() {
